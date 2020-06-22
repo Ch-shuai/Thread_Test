6b140649c8d23528e180feafcf1d1f93376b1466
wuzhanhao <1282591110@qq.com>
2020-06-22 14:09:11 +0800

    test

---
+++ src/lambda/LambdaDemo1.java
@@ -0,0 +1,13 @@
+package lambda;
+
+/**
+ * @author wuzhanhao
+ *  lambda
+ */
+public class LambdaDemo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/StudyTest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class StudyTest {
+
+    public static void main(String[] args) {
+        int i = 100 ;
+        while (true){
+            if (i<10){
+                break;
+            }
+            i = i-10;
+        }
+        System.out.println(i);
+    }
+}
